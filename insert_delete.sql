0a825fdf1002b18ba28b830c3abe839ef4ca6ca4
Jiamou Sun <u5871153@anu.edu.au>
2018-09-07 11:02:22 +1000

    add in/de.sql

---
+++ insert_delete.sql
@@ -0,0 +1,28 @@
+do
+$$
+DECLARE
+	counter INTEGER := 0;
+	pri_key VARCHAR := '0';
+	letter VARCHAR := 'A';
+BEGIN
+
+	LOOP
+	EXIT WHEN counter = 10000;
+		counter := counter + 1;
+		INSERT INTO basics VALUES(pri_key, CONCAT('tvEpisode', pri_key), '', '', false, 0, '', '130', CONCAT('{', 'Action', ',', 'Adventure', ',', 'Animation', ',',  letter, '}') :: VARCHAR[]);
+		pri_key := CAST(CAST(pri_key AS INTEGER) + 1 AS VARCHAR);
+		letter := CHR(counter % 26 + 65);
+	END LOOP;
+	
+	counter := 0;
+	pri_key := '0';
+	
+	LOOP
+	EXIT WHEN counter = 10000;
+		counter := counter + 1;
+		DELETE FROM basics WHERE tconst = pri_key;
+		pri_key := CAST(CAST(pri_key AS INTEGER) + 1 AS VARCHAR);
+	END LOOP;
+
+END;
+$$;
